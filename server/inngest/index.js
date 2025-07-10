6bf3094dcca8ec3ca2727ee132a58bf43d8887d5
Aman11102005 <aj652708@gmail.com>
2025-07-10 15:05:49 +0530

    new changes

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -4,16 +4,23 @@ import User from "../models/User.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
+// Helper to safely extract user data
+function extractUserData(event) {
+  // Log the event for debugging
+  console.log("Inngest event received:", JSON.stringify(event, null, 2));
+  // Try to get data from event.data or event.data.data or event.data.object
+  let data = event.data || event?.data?.data || event?.data?.object || event?.data?.user;
+  if (!data) throw new Error("Event data is undefined");
+  return data;
+}
+
 // Inngest Function to save user data to a database
 const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async (event) => {
-    if (!event.data) {
-      throw new Error("Event data is undefined");
-    }
-    const { id, first_name, last_name, email_addresses, image_url } =
-      event.data;
+    const data = extractUserData(event);
+    const { id, first_name, last_name, email_addresses, image_url } = data;
     const userdata = {
       _id: id,
       email: email_addresses[0].email_address,
@@ -29,10 +36,8 @@ const syncUserDeletion = inngest.createFunction(
   { id: "delete-user-from-clerk" },
   { event: "clerk/user.deleted" },
   async (event) => {
-    if (!event.data) {
-      throw new Error("Event data is undefined");
-    }
-    const { id } = event.data;
+    const data = extractUserData(event);
+    const { id } = data;
     await User.findByIdAndDelete(id);
   }
 );
@@ -41,10 +46,8 @@ const syncUserDeletion = inngest.createFunction(
 const syncUserUpdation = inngest.createFunction(
   { id: "update-user-from-clerk" },
   { event: "clerk/user.updated" },
-  async ({ data }) => {
-    if (!data) {
-      throw new Error("Event data is undefined");
-    }
+  async (event) => {
+    const data = extractUserData(event);
     const { id, first_name, last_name, email_addresses, image_url } = data;
     const userdata = {
       _id: id,
