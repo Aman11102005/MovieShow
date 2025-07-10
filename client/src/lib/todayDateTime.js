19b592395d732d079d6af5cea34c6ae858dd81b4
Aman11102005 <aj652708@gmail.com>
2025-07-10 13:10:29 +0530

    first commit

---
+++ client/src/lib/todayDateTime.js
@@ -0,0 +1,6 @@
+export const todayDateTime = () => {
+  return new Date(new Date().getTime - new Date().getTimezoneOffset() * 60000)
+    .toISOString()
+    .slice(0, 16);
+};
+
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeaturedSection from '../components/FeaturedSection'
+import TailersSection from '../components/TailersSection'
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection />
+      <FeaturedSection />
+      <TailersSection />
+    </div>
+
+    
+  )
+}
+
+export default Home
