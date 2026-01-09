feeee7628d97234e054691f14a8ceee6a771c7d3
Aman11102005 <amanjaiswalfea@gmail.com>
2026-01-09 11:25:53 +0530

    fix time zone

--- client/src/lib/isoTimeFormat.js
+++ client/src/lib/isoTimeFormat.js
@@ -1,6 +1,6 @@
 const isoTimeFormat = (dateTime)=>{
     const date = new Date(dateTime);
-    const localTime = date.toLocaleDateString('en-US',{
+    const localTime = date.toLocaleString('en-US',{
         hour: '2-digit',
         minute: '2-digit',
         hour12: true,
